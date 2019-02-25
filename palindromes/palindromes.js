4b668c98072ce9d4c0fc9a8f96453bebc87a6de4
Mjnari <rich.mj1@gmail.com>
2019-02-25 08:23:18 -0600

    palindromes complete

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function() {
+const palindromes = function (palindrome) {
+    let j = 1;
+    palindrome = palindrome.toLowerCase();
+    for (let i = 0; i < palindrome.length / 2; i++) {
+        while (i < palindrome.length && !isLetter(palindrome.charAt(i)))
+            i++;
+        if (i == palindrome.length)
+            return true;
+        while (j < palindrome.length && !isLetter(palindrome.charAt(palindrome.length - j)))
+            j++;
+        if (j == palindrome.length)
+            return true;
+        if (palindrome.charAt(i) != palindrome.charAt(palindrome.length - j)) {
+            return false;
+        }
+        j++;
+    }
+    return true;
+}
 
+function isLetter(str) {
+    return str.match(/[a-z]/i);
 }
 
 module.exports = palindromes
