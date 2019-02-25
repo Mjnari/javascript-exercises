c23202f25c82a425a3b25963bffced83501a8c43
Mjnari <rich.mj1@gmail.com>
2019-02-24 22:01:57 -0600

    calculator finished

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,40 @@
-function add () {
-	
+function add (x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum (numArray) {
+	let calcSum = 0;
+	for(let i = 0; i < numArray.length; i++)
+		calcSum += numArray[i];
+	return calcSum;
 }
 
-function multiply () {
-	
+function multiply (numArray) {
+	let calcProduct = 0;
+	if (numArray.length > 0)
+		calcProduct = 1;
+	for(let i = 0; i < numArray.length; i++)
+		calcProduct = calcProduct * numArray[i];
+	return calcProduct;
 }
 
-function power() {
-	
+function power(x, y) {
+	return x ** y;
 }
 
-function factorial() {
-	
+function factorial(x) {
+	if(x == 0)
+		return 1;
+	let product = 1;
+	while(x > 0){
+		product = product * x;
+		x--;
+	}
+	return product;
 }
 
 module.exports = {
