6e0220bdbfbf76418127a53b50751ec888444354
Mjnari <rich.mj1@gmail.com>
2019-02-24 22:11:39 -0600

    fibonacci complete

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(memberNumber) {
+    if(memberNumber < 0)
+        return 'OOPS';
 
+    let i = 1;
+    let sum = 1;
+    let lastSum = 1;
+    while(i < memberNumber - 1){
+        sum = sum + lastSum;
+        lastSum = sum - lastSum;
+        i++;
+    }
+    return sum;
 }
 
 module.exports = fibonacci
