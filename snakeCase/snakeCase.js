32ecc2a379757861483e5fca857bb5be0d2c1150
Mjnari <rich.mj1@gmail.com>
2019-03-08 15:31:57 -0600

    Update snakeCase.js

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,21 @@
-const snakeCase = function() {
+const snakeCase = function(phrase) {
+    phrase = phrase.toLowerCase();
+    let snakePhrase = "";
 
+    for(let i = 0; i < phrase.length(); i++){
+        if(isSpace(phrase.charAt(i)))
+            snakePhrase += "\_";
+        if(isLetter(phrase.charAt(i)))
+            snakePhrase += phrase.charAt(i);
+    }
+}
+
+function isSpace(str){
+    return str.match(" ");
+}
+
+function isLetter(str) {
+    return str.match(/[a-z]/i);
 }
 
 module.exports = snakeCase
